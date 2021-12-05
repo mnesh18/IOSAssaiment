2fe3df077c64a6de2f573878efa4614ecdc10054
muna <muna.alharbi.90@gmail.com>
2021-12-06 02:19:15 +0300

    up

---
+++ Profile Card/Profile Card/Profile_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_CardApp.swift
+//  Profile Card
+//
+//  Created by M's MacBook  on 08/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Profile_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Profile Card/Profile CardTests/Profile_CardTests.swift
@@ -0,0 +1,33 @@
+//
+//  Profile_CardTests.swift
+//  Profile CardTests
+//
+//  Created by M's MacBook  on 08/11/2021.
+//
+
+import XCTest
+@testable import Profile_Card
+
+class Profile_CardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
